39da1666b0b4b8d05b8657816fe93d6a46382410
DefineMyCode <118088290+DefineMyCode@users.noreply.github.com>
2022-11-30 15:15:39 +0800

    Add files via upload

---
+++ BinaryTree/Tree.c
@@ -0,0 +1,40 @@
+#include "Tree.h"
+
+//Ç°Ğò
+void PrevOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	printf("%c ", root->data);
+	PrevOrder(root->left);
+	PrevOrder(root->right);
+}
+
+//ÖĞĞò
+void InOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	InOrder(root->left);
+	printf("%c ", root->data);
+	InOrder(root->right);
+}
+
+//ºóĞò
+void PostOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	PostOrder(root->left);
+	PostOrder(root->right);
+	printf("%c ", root->data);
+}
